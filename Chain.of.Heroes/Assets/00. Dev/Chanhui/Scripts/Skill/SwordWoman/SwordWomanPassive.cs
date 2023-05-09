79c6b5910b870c918612a6d1c80ba629d651f546
20181290 한찬희 <20181290@vision.hoseo.edu>
2023-05-09 21:33:20 +0900

    Merge branch 'chan' of https://github.com/stayr8/Chain_Of_Heroes into chan

--- Chain.of.Heroes/Assets/00. Dev/Chanhui/Scripts/Skill/SwordWoman/SwordWomanPassive.cs
+++ Chain.of.Heroes/Assets/00. Dev/Chanhui/Scripts/Skill/SwordWoman/SwordWomanPassive.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class CharacterSwordWoman : CharacterBase
+public class SwordWomanPassive : CharacterBase
 {
     
     // 여기에 스킬 구현할 것.
