738220fd6180023ad39a990bd7ef11f51a151ca6
20181290 한찬희 <20181290@vision.hoseo.edu>
2023-05-15 22:32:38 +0900

    .

--- Chain.of.Heroes/Assets/00. Dev/Chanhui/Scripts/Action/AttackAction/RookAction.cs
+++ Chain.of.Heroes/Assets/00. Dev/Chanhui/Scripts/Action/AttackAction/RookAction.cs
@@ -111,7 +111,7 @@ public class RookAction : BaseAction
                 float rotateSpeed2 = 20f;
                 transform.forward = Vector3.Lerp(transform.forward, aimDir2, Time.deltaTime * rotateSpeed2);
 
-                float stoppingDistance1 = 3.5f;
+                float stoppingDistance1 = 0.1f;
                 if (Vector3.Distance(transform.position, targetDirection2) > stoppingDistance1)
                 {
                     float moveSpeed = 15f;
