d85a8dd72365073778de96eb1c0a2b0b6df2d6ab
junha0647 <junha0647@naver.com>
2023-05-22 03:31:36 +0900

    .

--- Chain.of.Heroes/Assets/00. Dev/Junha/Scenes/04. InGame/Scripts/PartyInfoCell.cs
+++ Chain.of.Heroes/Assets/00. Dev/Junha/Scenes/04. InGame/Scripts/PartyInfoCell.cs
@@ -10,13 +10,11 @@ public class PartyInfoCell : MonoBehaviour
 
     [SerializeField] private Image Img;
     [SerializeField] private TMP_Text Txt_Name;
-    [SerializeField] private TMP_Text Txt_Class;
-    [SerializeField] private TMP_Text Txt_Type;
+    [SerializeField] private TMP_Text Txt_ClassType;
     [SerializeField] private TMP_Text Txt_Level;
     [SerializeField] private TMP_Text Txt_AP;
     [SerializeField] private TMP_Text Txt_CAP;
     [SerializeField] private TMP_Text Txt_DP;
-
 
     void Awake()
     {
@@ -28,8 +26,7 @@ public class PartyInfoCell : MonoBehaviour
         if (unit == null) { return; }
 
         Txt_Name.text = unit.GetCharacterDataManager().m_name;
-        Txt_Class.text = unit.GetCharacterDataManager().m_class;
-        //Txt_Type.text = unit.GetCharacterDataManager().m_type;
+        Txt_ClassType.text = unit.GetCharacterDataManager().m_class;
         Txt_Level.text = unit.GetCharacterDataManager().m_level.ToString();
         Txt_AP.text = unit.GetCharacterDataManager().m_attackPower.ToString();
         Txt_CAP.text = unit.GetCharacterDataManager().m_chainAttackPower.ToString();
