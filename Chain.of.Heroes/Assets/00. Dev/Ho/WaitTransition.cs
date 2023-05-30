9606d3da59521be46ea66def99254af3d36f964a
junha0647 <junha0647@naver.com>
2023-05-30 10:33:58 +0900

    .

--- Chain.of.Heroes/Assets/00. Dev/Ho/WaitTransition.cs
+++ Chain.of.Heroes/Assets/00. Dev/Ho/WaitTransition.cs
@@ -11,8 +11,8 @@ public class WaitTransition : MonoBehaviour
 
     private IEnumerator goToScene()
     {
-        //yield return new WaitForSeconds(31f);
-        yield return new WaitForSeconds(1f);
+        yield return new WaitForSeconds(31f);
+        //yield return new WaitForSeconds(1f);
         LoadingSceneController.LoadScene("Ch_Tutorial"); // 튜토리얼 씬으로 연결해야 함
     }
 }
